9f1252ad4c1d20bf5100f3f50896974b6365f660
Juan Romero <jdrjuan@gmail.com>
2022-11-03 13:08:45 -0300

    Configura Atlas para la persistencia de datos

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const PERSISTENCE_TYPE = {
 const config = {
     PORT: 8080,
     PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB,    // 'MEM', 'FILE', 'MONGODB'
-    MONGODB_CONNECTION_STR: 'mongodb://localhost/ecommerce',
-    // MONGODB_CONNECTION_STR: 'mongodb+srv://juanromeroclases:HolaHola123@cluster0.5wxsavc.mongodb.net/ecommerce?retryWrites=true&w=majority',
+    // MONGODB_CONNECTION_STR: 'mongodb://localhost/ecommerce',
+    MONGODB_CONNECTION_STR: 'mongodb+srv://juanromeroclases:HolaHola123@cluster0.5wxsavc.mongodb.net/ecommerce?retryWrites=true&w=majority',
     MONGODB_TIMEOUT: 2000,  // Valor bajo para TESTING
 };
 
